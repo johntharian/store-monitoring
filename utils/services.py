d0b42b82ad6685379709cd3d7a365ca9b80df76b
John Tharian <jtharian13@gmail.com>
2023-11-01 21:45:29 +0530

    added docstrings

--- utils/services.py
+++ utils/services.py
@@ -10,6 +10,16 @@ from .report_optimized import get_uptime_downtime
 
 
 def create_report(db: Session, report_id: str):
+    """
+    Create a report with the given data
+
+    Parameters
+    ----------
+    db : Session
+        database session
+    report_id : str
+        id of the report to create
+    """
     report_location = f"reports/{report_id}.csv"
     db_report = Reports(
         report_id=report_id, status="Running", report_location=report_location
@@ -21,6 +31,19 @@ def create_report(db: Session, report_id: str):
 
 
 def trigger_report(db):
+    """
+    Trigger the creation of a new report.
+
+    Parameters
+    ----------
+    db : Session
+        database session
+
+    Returns
+    -------
+    str 
+        the id of the new report
+    """
     report_ids = db.query(Reports.report_id).all()
 
     report_id = "report" + str(len(report_ids) + 1)
@@ -32,6 +55,16 @@ def trigger_report(db):
 
 
 def generate_report(db: Session, report_id: str):
+    """
+    Generates the report.
+
+    Parameters
+    ----------
+    db : Session
+        database session
+    report_id : str
+        id of the report
+    """
     # get_uptime_downtime_last_day(db,report_id)
     get_uptime_downtime(db, report_id)
     # get_uptime_downtime_last_hour(db,report_id)
@@ -39,6 +72,21 @@ def generate_report(db: Session, report_id: str):
 
 
 def get_report(db: Session, report_id: str):
+    """
+    Get report from database.
+
+    Parameters
+    ----------
+    db : Session
+        database session
+    report_id : str
+        id of the report to retrieve
+
+    Returns
+    -------
+    str 
+        status of the report
+    """
     report = db.query(Reports).filter(Reports.report_id == report_id).first()
     if not report:
         return "No such report id"
@@ -55,6 +103,16 @@ def get_report(db: Session, report_id: str):
 
 
 def update_report_status(db: Session, report_id: str):
+    """
+    Updates status of the report when finished generating.
+
+    Parameters
+    ----------
+    db : Session
+        database session
+    report_id : str
+        id of the report to retrieve
+    """
     report = db.query(Reports).filter(Reports.report_id == report_id).first()
 
     if report:
